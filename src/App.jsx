b18a3fcc2fd54b446114a88abe761ef8763d22e6
siddharthbeniwal023 <siddharthbeniwal2001@gmail.com>
2023-07-07 22:46:13 +0530

    Blockbuster UI developed

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import Home from "./home/Home";
 import "./App.scss";
 import Watch from "./Watch/Watch";
 import Register from "./Register/Register";
+import Login from "./Login/Login";
 
 
 function App() {
   return (
     <div className="App">
       {/* <Home /> */}
-      <Register/>
+      <Login/>
     </div>
   );
 }
--- src/Register/Register.jsx
+++ src/Register/Register.jsx
@@ -1,6 +1,18 @@
+import { useRef, useState } from "react";
 import "./Register.scss";
 
 function Register() {
+  const [email, setEmail] = useState("");
+  const emailRef = useRef();
+  const handleStart = () => {
+    setEmail(emailRef.current.value);
+  };
+  const [password, setPassword] = useState("");
+  const passwordRef = useRef();
+  const handleFinish = () => {
+    setPassword(passwordRef.current.value);
+  };
+
   return (
     <div className="register">
       <div className="top">
@@ -9,11 +21,20 @@ function Register() {
       </div>
       <div className="container">
         <h1>Netflix ate us</h1>
-        <h2>So we giving movies for free</h2>
-        <div className="input">
-          <input type="email" placeholder="email address" />
-          <button className="start">Get Started</button>
-        </div>
+        <h2>So we giving movies for free :)</h2>
+        {!email ? (
+          <div className="input">
+            <input type="email" placeholder="email address" ref={emailRef} />
+            <button className="start" onClick={handleStart}>
+              Get Started
+            </button>
+          </div>
+        ) : (<form className="input">
+        <input type="password" placeholder="password" ref={passwordRef} />
+        <button className="start" onClick={handleFinish}>
+          Start
+        </button>
+      </form>) }
       </div>
     </div>
   );
